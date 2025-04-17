bb8b5bb8908aa2cd44c5eeb23d004917b4c274c5
ibrahim <ibrahimjp630@gmail.com>
2025-04-17 17:10:58 +0530

    Completed All Backend API's and All Necessary Stuff

--- app/page.js
+++ app/page.js
@@ -6,19 +6,28 @@ import gsap from "gsap";
 import { Button } from "../@/components/ui/button";
 gsap.registerPlugin(useGSAP);
 export default function Home() {
-  useGSAP(() => {
-    gsap.from("body", {
-      opacity: 0,
-      y: 30,
-      duration: 1,
-      delay: 0.6,
-    });
-  }, []);
-  
   return (
-    <div>
-      Hello
-      <Button>Button</Button>
+    <div className="flex justify-center items-center mt-50">
+<div class="card ">
+  <div class="banner">
+    <span class="banner-text">Let's</span>
+    <span class="banner-text">Get Started</span>
+  </div>
+  <span class="card__title">Ai Interview Preparation</span>
+  <p class="card__subtitle">Get Ready For Your Interviews.</p>
+    <a href="/dashboard">
+    <button class="sign-up">Start</button>
+    </a>
+</div>
+
+<video
+  className="background-video"
+  src="/anime.mp4"
+  autoPlay
+  loop
+  muted
+  playsInline
+/>
     </div>
   );
 }
